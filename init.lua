45cafcbf126e2f296dad53c4d16601535e4baaa7
Markus <Hello@Mackrics.com>
2023-08-30 19:44:20 +0200

    remove old code

--- init.lua
+++ init.lua
@@ -3,12 +3,7 @@ function get_file_content()  -- arguments: julia, r, python ...
 	local path = vim.api.nvim_buf_get_name(0) -- get path of current buffer
 	local file = io.open(path, r) -- load current file
 	local file_content = file:read("*a") -- get file content
-	-- could we instead print this as list?
-	-- gmatch output a function
-	-- calling matches() will return the next string each time
-	-- think of this as list of function
-	local matches = string.gmatch(file_content, "```{r}(.-)```") -- make flexible in language
-	-- placeholder values
+	local matches = string.gmatch(file_content, "```{r}(.-)```") -- make flexible in language or
 	local i = 0
 	local code_table = {}
 	for match in matches do
@@ -21,7 +16,6 @@ end
 
 -- Print available channels
 function get_channels()
-	-- the output here is https://neovim.io/doc/user/api.html#nvim_get_chan_info()
 	local channels = vim.api.nvim_list_chans()
 	local channel_table = {}
 	i = 0
