43b153abc72cbdb1c99e05557bc1a823e964c7b1
Markus <Hello@Mackrics.com>
2023-08-31 22:04:02 +0200

    factor out code, add function to send line and visual selection

--- init.lua
+++ init.lua
@@ -28,10 +28,47 @@ function get_channels()
 	return(channel_table)
 end
 
+-- send code from current paragraph
+function send_line()
+	local line = vim.api.nvim_get_current_line() .. "\r"
+	send_content(line)
+end
+
+
+-- send entire quarto file
+function send_quarto()
+	local code = get_file_content("```{r}", "```")
+	send_content(code)
+end
+
+-- send cell
+
+-- send current paragraph
+
+-- send visual selection
+
+-- need to understand this
+function get_visual_selection()
+  local s_start = vim.fn.getpos("'<")
+  local s_end = vim.fn.getpos("'>")
+  local n_lines = math.abs(s_end[2] - s_start[2]) + 1
+  local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
+  lines[1] = string.sub(lines[1], s_start[3], -1)
+  if n_lines == 1 then
+    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
+  else
+    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3])
+  end
+  return table.concat(lines, "\n\r")
+end
+
+function send_visual_selection()
+	local code = get_visual_selection()
+	send_content(code)
+end
 
 -- Pick channel
-function send_content()
-	local code = get_file_content("```{r}", "```")
+function send_content(code)
 	local all_channels = get_channels()
 	-- if channel is null, return error
 	-- if code is null, return error
