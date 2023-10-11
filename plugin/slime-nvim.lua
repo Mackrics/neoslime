379d96eb6f17908cc100659854c1ab358d23216f
Markus <Hello@Mackrics.com>
2023-10-11 19:40:57 +0200

    load in slime

--- plugin/slime-nvim.lua
+++ plugin/slime-nvim.lua
@@ -1,7 +1,7 @@
 --:h lua-guide-commands-create
 -- https://github.com/Shatur/neovim-session-manager/blob/68dde355a4304d83b40cf073f53915604bdd8e70/plugin/session_manager.lua
 
-local slime require("send-stuff")
+local slime = require('send-stuff')
 
 vim.api.nvim_create_user_command(
   'SlimeEdit', 
