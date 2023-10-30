1f4ddbed48fec0e47f62ec34082741bb06b4db61
Markus <Hello@Mackrics.com>
2023-10-30 20:29:20 +0100

    clean up function exporting

--- plugin/slime-nvim.lua
+++ plugin/slime-nvim.lua
@@ -1,11 +1,20 @@
 --:h lua-guide-commands-create
--- https://github.com/Shatur/neovim-session-manager/blob/68dde355a4304d83b40cf073f53915604bdd8e70/plugin/session_manager.lua
 
 local slime = require('send-stuff')
 
-vim.g.cell_delimitor = "```"
-
 vim.g.chosen_channel = nil
+
+vim.api.nvim_create_user_command(
+  'NeoSlimeEdit', 
+  'lua edit_channel()',
+  {}
+)
+
+vim.api.nvim_create_user_command(
+  'NeoSlimeEdit', 
+  'lua edit_channel()',
+  {}
+)
 
 vim.api.nvim_create_user_command(
   'NeoSlimeSendLine', 
@@ -14,31 +23,7 @@ vim.api.nvim_create_user_command(
 )
 
 vim.api.nvim_create_user_command(
-  'NeoSlimeSendBlock', 
-  'lua send_block()',
-  {}
-)
-
-vim.api.nvim_create_user_command(
-  'NeoSlimeSendCell', 
-  'lua send_cell()',
-  {}
-)
-
-vim.api.nvim_create_user_command(
   'NeoSlimeSendVisualSelection', 
   'lua send_visual_selection()',
   {range = 0}
-)
-
-vim.api.nvim_create_user_command(
-  'NeoSlimeSendQuarto', 
-  'lua send_quarto()',
-  {}
-)
-
-vim.api.nvim_create_user_command(
-  'NeoSlimeEdit', 
-  'lua edit_channel()',
-  {}
 )
