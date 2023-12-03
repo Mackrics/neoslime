d18ebb1f6bc6fff31a0dbb8345c10137cba6a5a4
Markus <Hello@Mackrics.com>
2023-12-03 18:36:15 +0100

    add default keybindings

--- plugin/slime-nvim.lua
+++ plugin/slime-nvim.lua
@@ -1,8 +1,11 @@
---:h lua-guide-commands-create
-
+-- source scripts to be loaded
 local slime = require('send-stuff')
 
+-- Default channel is nil
 vim.g.chosen_channel = nil
+
+-- use default keybindings by default
+vim.g.use_default_neoslime_keybinds = true
 
 vim.api.nvim_create_user_command(
   'NeoSlimeEdit', 
@@ -27,3 +30,11 @@ vim.api.nvim_create_user_command(
   'lua send_visual_selection()',
   {range = true}
 )
+
+-- Default keybindings
+if vim.g.use_default_neoslime_keybinds = true then
+  vim.keymap.set("n", "<leader>s",  "?^#.*----<Cr>jV/^#.*----<Cr>k:'<,'> NeoSlimeSendVisualSelection<Cr>n")
+  vim.keymap.set({"n", "i"}, "<C-h>", "<Cmd>NeoSlimeSendLine<Cr>")
+  vim.keymap.set("v", "<C-h>",  ":'<,'> NeoSlimeSendVisualSelection<Cr>")
+  vim.keymap.set({"n", "v"}, "<leader>e", "<Cmd>NeoSlimeEdit<Cr>")
+end
