68523244e0b6c881876bee28a1a40b2a1a59a30d
Markus <Hello@Mackrics.com>
2024-02-11 11:14:19 +0100

    rename plugin file

--- plugin/neoslime.lua
+++ plugin/neoslime.lua
@@ -36,6 +36,12 @@ vim.api.nvim_create_user_command(
   {range = true}
 )
 
+vim.api.nvim_create_user_command(
+  'NeoSlimePrint', 
+  'lua print_channel()',
+  {}
+)
+
 -- Default keybindings
 if vim.g.use_default_neoslime_keybinds == true then
   vim.keymap.set("n", "<leader>s",  "?^```<Cr>jV/^```<Cr>k:'<,'> NeoSlimeSendVisualSelection<Cr>n")
